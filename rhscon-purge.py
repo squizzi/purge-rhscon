6c157d32e8d3c741527bf52697abe906fde5d178
Kyle Squizzato <ksquizza@redhat.com>
2017-03-23 22:44:32 -0400

    Built out the rest of the functions, added more print for user context
    Signed-off-by: Kyle Squizzato <ksquizza@redhat.com>

--- rhscon-purge.py
+++ rhscon-purge.py
@@ -23,8 +23,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import dbus
+import sys
 import os
+import dbus
+import socket
+import signal
+import atexit
+import argparse
 from shutil import rmtree
 from subprocess import Popen, PIPE
 
@@ -36,7 +41,7 @@ class colors:
     OKBLUE = '\033[94m'     # blue
     OKGREEN = '\033[92m'    # green
     WARNING = '\033[93m'    # yellow
-    FAIL = '\033[91m'       # red
+    ERROR = '\033[91m'      # red
     ENDC = '\033[0m'        # white
     BOLD = '\033[1m'        # bold
     UNDERLINE = '\033[4m'   # underline
@@ -60,7 +65,10 @@ def print_message(message, state=None, color=None):
             raise
     # if no state is supplied just build message with provided color
     if state == None:
-        state = ''Popen
+        state = ''
+    else:
+        # else uppercase it
+        state = state.upper()
     constructed_state = color + ('[{0}] '.format(state))
     constructed_message = constructed_state + colors.ENDC + str(message)
     return constructed_message
@@ -130,9 +138,9 @@ Check for root, exit if non-root
 """
 def need_root():
     if os.geteuid() != 0:
-    print pm('Root privileges are required to run this script, please re-run \
+        print pm('Root privileges are required to run this script, please re-run \
 with root to continue', 'error', colors.ERROR)
-    sys.exit(1)
+        sys.exit(1)
 
 """
 Validate given hostname
@@ -184,13 +192,17 @@ Given a host_list, clean each storage node
 def clean_nodes(host_list):
     storage_clean_command = 'systemctl stop salt-minion ; rm -f /etc/salt/pki/minion'
     for each in host_list:
+        # We shouldn't need keyless ssh as it should already be configured if
+        # RHSC had already been running/deployed
+        # If keyless ssh doesn't work on nodes then we'll just ask users to
+        # set it up manually
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.load_system_host_keys()
         print pm('Connecting to host: {0} to clean storage node configuration'.format(each),
                  'info', colors.INFO)
         ssh.connect(each,
-                    username="root"
+                    username="root",
                     look_for_keys=False
                     )
         stdin, stdout, stderr = ssh.exec_command(storage_clean_command)
@@ -202,7 +214,6 @@ def clean_nodes(host_list):
             print pm('Error cleaning node: {0}'.format(each), 'error',
                      colors.ERROR)
             ssh.close()
-
 
 """
 Clean the mongodb
@@ -229,14 +240,55 @@ def clean_db(scriptfile):
 """
 Bootstrap client agent(s)
 """
+def bootstrap_nodes(host_list, server_fqdn):
+    bootstrapCommand = 'yum update -y ; systemctl start ntpd ; curl {0}:8181/setup/agent | bash'.format(server_fqdn)
+    # Reopen the ssh connection so we can connect to each host in the lineup
+    ssh.close()
+    for each in host_list:
+        print pm('Reinitializing storage node: {0}'.format(each), 'info', colors.INFO)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(each,
+                    username="root",
+                    look_for_keys=False
+                    )
+        stdin, stdout, stderr = ssh.exec_command(bootstrapCommand)
+        exit_status = stdout.channel.recv_exit_status()
+        if exit_status == 0:
+            print pm('Storage node: {0} initialized'.format(each),
+                     'complete', colors.OKGREEN)
+        else:
+            print "\n[ERROR] during bootstrap of host {0}. {1}".format(each, exit_status)
+            client.close()
+            sys.exit(1)
+    print pm('The client agent has been installed and configured but may take a few moments to appear in the RHCS Web UI',
+             'complete', colors.OKGREEN)
 
+"""
+exit handler
+"""
+def exit_handler():
+    pass
+    #print pm('rhscon-purge has exited', 'info', colors.INFO)
+
+"""
+Super simple signal handler
+"""
+SIGNALS_TO_NAMES_DICT = dict((getattr(signal, n), n) \
+    for n in dir(signal) if n.startswith('SIG') and '_' not in n )
+
+def signal_handler(signum, frame, retries=0):
+    print pm("Received signal: {0}({1})".format(SIGNALS_TO_NAMES_DICT[signum], signum),
+             'info', colors.INFO)
+    raise RuntimeError('Received signal: {0}({1})'.format(SIGNALS_TO_NAMES_DICT[signum], signum))
+    sys.exit(signum)
 
 def main():
     parser = argparse.ArgumentParser(description='Purge an existing Red Hat \
                                      Storage Console 2.0 configuration.  This \
                                      script should be ran on the offending \
                                      RHSC host. This script also \
-                                     re-initializes the storage \
+                                     reinitializes the storage \
                                      nodes configured to use the RHSC host.')
     parser.add_argument("-n",
                         "--nodes",
@@ -245,14 +297,22 @@ def main():
                         currently serve as nodes in a cluster.  rhscon-purge \
                         will attempt to determine nodes associated with an RHSC \
                         automatically.  This option should only be used to \
-                        specify additional nodes that need to be cleaned.')
+                        specify additional nodes that need to be cleaned, that \
+                        did not clean successfully following automated \
+                        cleaning.')
     parser.add_argument("--no-node-clean",
-                        dest="node_clean",
+                        dest="nonode_clean",
                         action='store_true',
                         help='Do not clean the storage nodes, only clean the \
                         local configuration from the RHSC node.')
 
     args = parser.parse_args()
+
+    # Check to see how we need to generate a host_list
+    if args.nodes != None:
+        host_list = build_host_list(args.nodes)
+    else:
+        host_list = generate_hosts()
 
     # Steps based on guide from https://access.redhat.com/solutions/2944461
     # Stop skyring and salt-master on RHSC node (where script runs)
@@ -260,6 +320,45 @@ def main():
     manage_service('stop', 'salt-master')
 
     # On all given storage nodes, stop salt-minion services and remove salt-keys
+    if args.nonode_clean != True:
+        clean_nodes(host_list)
+    else:
+        # Don't clean if nonode_clean is set
+        print pm('--no-node-clean is set, skipping storage node cleaning',
+                 'info', colors.INFO)
+        pass
 
     # Clean the mongodb on the RHSC node
     clean_db('cleaner.js')
+
+    # Remove minions directories
+    remove_dir('/etc/salt/pki/master/minions')
+
+    # Start skyring and salt-master
+    manage_service('start', 'skyring')
+    manage_service('start', 'salt-master')
+
+    # Bootstrap client agents
+    if args.nonode_clean != True:
+        server = socket.gethostname()
+        bootstrap_nodes(host_list, server)
+    else:
+        # Don't bootstrap if nonode_clean is set
+        print pm('--no-node-clean is set, skipping storage node bootstrap',
+                 'info', colors.INFO)
+        pass
+
+    # Print a completion message and exit
+    print pm('Purge complete', 'complete', colors.OKBLUE)
+    sys.exit(0)
+
+"""
+Main
+"""
+if __name__ == '__main__':
+    atexit.register(exit_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGHUP, signal_handler)
+    signal.signal(signal.SIGQUIT, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+    sys.exit(main())
